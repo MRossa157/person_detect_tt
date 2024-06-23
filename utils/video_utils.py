18d7198bad30d6ad321d3256231d778bb584926c
MRossa <maxim.rossa@mail.ru>
2024-06-24 01:39:39 +0400

    FPS fix

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -15,7 +15,7 @@ def read_video(video_path):
 
 def save_video(output_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    out = cv2.VideoWriter(output_video_path, fourcc, 60, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
     for frame in tqdm(output_video_frames, desc="Сохраняем видео по-кадрово"):
         out.write(frame)
     out.release()
